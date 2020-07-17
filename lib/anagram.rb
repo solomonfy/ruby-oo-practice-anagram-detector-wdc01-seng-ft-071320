be393915de1c97c8017e22651a35402c790599ea
solomonfy <fantahunsolomon@gmail.com>
2020-07-17 03:12:36 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :word, :match
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.select {|w| w.split("").sort == self.word.split("").sort}
+    end
+    
+end
